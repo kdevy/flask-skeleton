5a4ca78faa03131566b024c57e8887cf7b7b6bc7
kdevy <ykinoshita.cal0@gmail.com>
2025-07-15 04:30:22 +0900

    formwtf導入

--- app/views/login_view.py
+++ app/views/login_view.py
@@ -1,15 +1,27 @@
 from flask import request, jsonify, render_template
 from flask.views import MethodView
 from app.services.user_service import UserService
+from app.forms.login_form import LoginForm
 
 class LoginView(MethodView):
+    form = None
+
+    def dispatch_request(self, *args, **kwargs):
+        self.form = LoginForm()
+        return super().dispatch_request(*args, **kwargs)
+
     def get(self):
-        context = {}
+        context = {"form": self.form}
         return render_template("login.html", **context)
-    
+
     def post(self):
-        data = request.form
+        data = None
         try:
+            if self.form.validate_on_submit():
+                data = self.form.data
+            else:
+                raise ValueError("Invalid form submission")
+
             user = UserService.login(data.get("username"), data.get("password"))
             return jsonify({"message": "Logged in", "user": user})
         except ValueError as e:

38aefa2716b945364882f595451b14cc9fe7dd7d
kdevy <ykinoshita.cal0@gmail.com>
2025-12-02 00:43:04 +0900

    トップのサンプルページ追加

--- app/views/index/index_view.py
+++ app/views/index/index_view.py
@@ -1,8 +1,30 @@
-from flask import request, jsonify, render_template
+from flask import render_template
 from flask.views import MethodView
-from app.services.user_service import UserService
+from flask import current_app, url_for
 
 class IndexView(MethodView):
+
+    def list_routes(self):
+        routes = []
+        for rule in current_app.url_map.iter_rules():
+            if rule.endpoint == "static":
+                continue
+
+            methods = sorted(m for m in rule.methods if m not in ("HEAD", "OPTIONS"))
+
+            dummy_kwargs = {arg: f"<{arg}>" for arg in rule.arguments}
+            try:
+                url = url_for(rule.endpoint, **dummy_kwargs)
+            except Exception:
+                url = str(rule)
+
+            routes.append({
+                "endpoint": rule.endpoint,
+                "rule": str(rule),
+                "url": url,
+                "methods": methods,
+            })
+        return routes
     def get(self):
-        context = {}
+        context = {"routes": self.list_routes()}
         return render_template("index.html", **context)

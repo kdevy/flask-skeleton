faef57ef6733d690ee579528cfa57c468200144f
kdevy <ykinoshita.cal0@gmail.com>
2025-12-05 17:22:13 +0900

    add user bp

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,9 @@ env_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
 load_dotenv(env_file_path)
 
 from flask import Flask
-from myflaskbase.extensions import db
+from myflaskbase.extensions import db, login_manager
 from myflaskbase import init_app
+from app.user.models import User
 from app.config import ProjectConfig
 
 def create_app(config=ProjectConfig):
@@ -17,7 +18,15 @@ def create_app(config=ProjectConfig):
     with app.app_context():
         db.create_all()
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    login_manager.login_view = "user.login"
+
     from app.views import index_bp
     app.register_blueprint(index_bp, url_prefix="/")
+    from app.user import user_bp
+    app.register_blueprint(user_bp)
 
     return app
